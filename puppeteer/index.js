81b9ae7595f5bdbc0fd47f215d40fee0fcf30832
august <augustherrstrom@gmail.com>
2025-07-04 16:37:45 +0200

    test generate pdf

---
+++ puppeteer/index.js
@@ -0,0 +1,16 @@
+import puppeteer from "puppeteer";
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://cv.aghost.io", {
+    waitUntil: "networkidle2",
+  });
+
+  await page.pdf({
+    path: "August-Herrstrom-CV.pdf",
+    format: 'A4'
+  });
+
+  await browser.close();
+})();

7b3446564bc6b48f6f0528d874b119044fd2b9e5
august <augustherrstrom@gmail.com>
2025-07-04 16:46:03 +0200

    try setting chrome channel

--- puppeteer/index.js
+++ puppeteer/index.js
@@ -1,7 +1,7 @@
 import puppeteer from "puppeteer";
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({browser: 'chrome', channel: 'stable'});
   const page = await browser.newPage();
   await page.goto("https://cv.aghost.io", {
     waitUntil: "networkidle2",
